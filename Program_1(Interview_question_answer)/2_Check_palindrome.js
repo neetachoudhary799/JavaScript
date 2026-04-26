0a1f67cfc2ae33acf693bfaf296bc4d9c2268c3f
neeta <neeta.rani@mail.vinove.com>
2026-04-26 22:32:01 +0530

    check Palidrome ...

--- Program_1(Interview_question_answer)/2_Check_palindrome.js
+++ Program_1(Interview_question_answer)/2_Check_palindrome.js
@@ -32,3 +32,25 @@ function checkPalidrome2(string){
     return acc;
   }, true);
 }
+
+//solution 3
+console.log(checkPalidrome3(string));
+function checkPalidrome3(string){
+    let left = 0;
+    let right = string.length - 1;
+    while(left < right){
+        if(string[left] !== string[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+//solution 4
+console.log(checkPalidrome4(string));
+function checkPalidrome4(string){
+    let reverse = string.split('').reverse().join('');
+    return string === reverse;
+}
