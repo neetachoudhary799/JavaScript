3b04c72609cebd46b613d34058f3bf98bd7fddea
neeta <neeta.rani@mail.vinove.com>
2026-04-28 18:22:26 +0530

    basics ...

---
+++ Basics/a_javascript.js
@@ -0,0 +1,13 @@
+//JavaScript is a programming language used to make web pages interactive and dynamic.
+//It can be used to create things like animations, form validation, and interactive maps.
+
+//Here is an example of a simple JavaScript code that displays an alert message when a button is clicked:   
+//HTML code for the button
+//<button onclick="myFunction()">Click me</button>    
+//JavaScript code for the function
+
+function myFunction() {
+  alert("Hello! You clicked the button.");
+}
+
+myFunction();
