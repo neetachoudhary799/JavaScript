ea23832346b39339247e26bb7b8738d93b21d282
neeta <neeta.rani@mail.vinove.com>
2026-04-29 18:52:57 +0530

    forloop.....

---
+++ Basics/e_forloop.js
@@ -0,0 +1,22 @@
+//forloop : A for loop is used to repeat a block of code multiple times.
+//Syntax: for (initialization; condition; increment/decrement) {
+//  // code to be executed
+//}
+
+//Example: Print numbers from 1 to 5
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//Example: Print even numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {        
+    console.log(i);
+    }
+}
+
+//Example: Print elements of an array
+let arr = ['a', 'b', 'c', 'd'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}   
