1e1339431117457120251a68ff0173705d8cd690
neeta <neeta.rani@mail.vinove.com>
2026-04-29 19:05:41 +0530

    forEach.....

---
+++ Basics/g_foreach.js
@@ -0,0 +1,18 @@
+//forEach :forEach() is used to loop through an array and execute a function for each element.
+
+//Syntax: array.forEach(callback(currentValue, index, array), thisArg)
+//callback: A function to execute on each element in the array. It takes three arguments:
+//  - currentValue: The current element being processed in the array.
+//  - index: The index of the current element being processed in the array.
+//  - array: The array forEach was called upon.
+
+//Note: The forEach() method does not return a value. It is used for its side effects, such as modifying the original array or performing actions based on the elements of the array.
+
+//we push the code on array and show values
+//❌ Cannot use break or continue
+
+//Example: Print each element of an array
+let arr = ['a', 'b', 'c', 'd'];
+arr.forEach((element) => {
+  console.log(element);
+});
