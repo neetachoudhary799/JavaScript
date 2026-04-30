7801635071c0c05e033b8bb38ff70b6ea5fc8118
neeta <neeta.rani@mail.vinove.com>
2026-04-30 12:11:03 +0530

    modern loop (of) or (in).....

--- Basics/f_moden_loop.js
+++ Basics/f_moden_loop.js
@@ -1,3 +1,9 @@
+//of: values
+//in: keys
+//for loop: best for arrays and strings
+//for...of: best for arrays and strings (values)
+//for...in: best for objects (keys)
+
 //for...of (best for arrays)  //show values //no return values for objects ,we push values in array and show values
 for (let num of [1, 2, 3]) {
   console.log(num);
@@ -17,3 +23,18 @@ for (let key in obj) {
   console.log(obj[key]);
 }
 //Output: 1, 2, 3
+
+
+//stringfor...of (best for strings)  //show values
+let str = "Hello";
+for (let char of str) {
+  console.log(char);
+}
+//Output: H, e, l, l, o
+
+//strindfor...in (best for strings)  //show keys
+let str = "Hello";  
+for (let index in str) {
+  console.log(index);
+}
+//Output: 0, 1, 2, 3, 4
