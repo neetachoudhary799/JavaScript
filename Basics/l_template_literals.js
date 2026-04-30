279187ae3a50c0d92acdcda711be261c5da1d95a
neeta <neeta.rani@mail.vinove.com>
2026-04-30 13:19:09 +0530

    template Literals.....

---
+++ Basics/l_template_literals.js
@@ -0,0 +1,8 @@
+//template literals
+//Template literals are a modern way to work with strings using backticks ( ) instead of quotes.
+//we can write one linecode it is easier to read and write multiline strings without needing escape characters for newlines or quotes.
+
+let name = "Alice";
+let age = 30;
+let message = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message); // Output: "Hello, my name is Alice and I am 30 years old."
