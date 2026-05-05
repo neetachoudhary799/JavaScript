46172f84861d5a1bd87e6845008e4449a799e162
neeta <neeta.rani@mail.vinove.com>
2026-05-05 15:43:26 +0530

    Promise API..

--- Advanced_JS_Level_1/c_promice_ap1.js
+++ Advanced_JS_Level_1/c_promice_ap1.js
@@ -1,8 +1,8 @@
 //primise API:
-//1)settled
-//2)allsetled
-//3)race
-//4)any
+//1)all :=> it will wait for all the promises to be resolved or rejected and then it will return the result in an array. if any of the promise is rejected then it will return the error message.
+//2)allsetled:=> it will wait for all the promises to be resolved or rejected and then it will return the result in an array with status and value or reason.
+//3)race :=> it will return the result of the first promise that is resolved or rejected.
+//4)any  :=> it will return the result of the first promise that is resolved. if all the promises are rejected then it will return the error message.
 
 //settled
 let promise1 = new Promise((resolve, reject) => {
@@ -17,6 +17,7 @@ let promise2 = new Promise((resolve, reject) => {
     }, 1000);   
 });
 
+//allSettled
 Promise.allSettled([promise1, promise2])
 .then(results => {
     results.forEach((result, index) => {
@@ -33,7 +34,7 @@ Promise.allSettled([promise1, promise2])
 //Promise 2 rejected with reason: Promise 2 rejected
 
 
-//allsetled
+//all
 Promise.all([promise1, promise2])
 .then(results => {
     console.log("All promises resolved:", results); 
