dd64bffd04325d406032fcc6c609e31322b2a433
neeta <neeta.rani@mail.vinove.com>
2026-05-05 20:36:09 +0530

    IIFE.....

---
+++ Advanced_JS_Level_1/l_IIFE.js
@@ -0,0 +1,23 @@
+//👉 Immediately Invoked Function Expression
+//👉 A function that runs immediately after it is defined
+
+(function() {
+    console.log("Hello IIFE");
+})();
+
+//👉 Avoid global scope pollution
+//👉 Create private variables
+//👉 Run code immediately
+
+
+//👉 Create private variables
+(function() {
+    let name = "Neeta";
+    console.log(name);
+})();
+
+// console.log(name); ❌ Error (private)
+
+(function(name) {
+    console.log("Hello " + name);
+})("Neeta");
