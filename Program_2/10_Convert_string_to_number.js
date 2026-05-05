321866408aebb02b69d7a32d538a97cb503d883a
neeta <neeta.rani@mail.vinove.com>
2026-05-05 10:13:40 +0530

    Convert String to number...

---
+++ Program_2/10_Convert_string_to_number.js
@@ -0,0 +1,32 @@
+let string="6";
+
+//solution 1
+let number=Number(string);
+console.log(typeof(number));
+console.log(number);
+
+//solution 2
+let number2=parseInt(string);
+console.log(typeof(number2));
+console.log(number2);
+
+//solution 3
+let number3=+string;
+console.log(typeof(number3));
+console.log(number3);   
+
+//solution 4
+let number4=string*1;
+console.log(typeof(number4));
+console.log(number4);
+
+//solution 5
+let number5=string/1;
+console.log(typeof(number5));
+console.log(number5);   
+
+//solution 6
+let number6=string-0;
+console.log(typeof(number6));
+console.log(number6);
+
