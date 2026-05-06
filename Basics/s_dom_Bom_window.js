37a16f357ec828fb107f542bc8b9f532151cd96d
neeta <neeta.rani@mail.vinove.com>
2026-05-06 16:15:56 +0530

    dom bom and window .....

---
+++ Basics/s_dom_Bom_window.js
@@ -0,0 +1,30 @@
+//DOM: DOM (Document Object Model)
+//👉 It is a programming interface for HTML and XML document
+// it is complete page make a object is called DOM
+//👉 It allows us to manipulate the structure, style, and content of a web page using JavaScript
+//Works with HTML elements
+//Starts from document
+//Used for UI manipulation
+
+//document.getElementById("title").innerText = "Hello Neeta";
+
+//BOM: BOM (Browser Object Model)
+//👉 It is a programming interface for browser objects
+//👉 It allows us to interact with the browser and its features using JavaScript
+//URL
+//browser window
+//history
+//screen
+
+//console.log(window.location.href); // current URL
+//window.alert("Hello!");
+
+//Important: window is global object
+// The top-level object in browser
+//Everything comes under window
+//window
+ //├── document (DOM)
+ //├── location (BOM)
+ //├── navigator (BOM)
+ //├── alert(), setTimeout()
+ //window.document.title = "My Page";
