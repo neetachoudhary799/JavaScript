caed4ce56bfdc23b7b51ee335220e0ae3392f03b
neeta <neeta.rani@mail.vinove.com>
2026-05-06 15:23:37 +0530

    while and do while.....

---
+++ Basics/o_while_and_dowhile.js
@@ -0,0 +1,17 @@
+//While : Runs only if condition is true
+//👉 Condition checked before execution
+//while → when condition is known
+let i=1;
+while(i<=5){
+    console.log(i)
+    i++;
+}
+
+//Do while : Runs at least once
+//do...while → when code must run at least once
+//👉 Condition checked after execution
+let j=1;
+do{
+    console.log(j);
+    j++;
+}while(j<=5);
