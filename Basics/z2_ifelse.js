e449c9e8bcdb082fb0fb46875990530e51d2429e
neeta <neeta.rani@mail.vinove.com>
2026-05-08 15:25:10 +0530

    if else condition ...

---
+++ Basics/z2_ifelse.js
@@ -0,0 +1,9 @@
+//ifelse : if condition is true then return outout otherwise it goes to else condition.
+
+let x=true;
+
+if(x){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
