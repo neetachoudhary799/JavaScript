7054212bfaaa1c01c945c65483a6371de5a435ce
neeta <neeta.rani@mail.vinove.com>
2026-05-14 10:16:00 +0530

    generator iterators...

---
+++ Medium/7_generator_iterators.js
@@ -0,0 +1,17 @@
+//iterator: An iterator is an object that allows sequential access to values.
+//done: true
+//means iteration finished ✔️
+//output shows {value:10,Done:true/fasle}
+
+let array = [1,2,3];
+let iterator= arr[Symbol.iterator]();
+
+console.log(iterator.next()); //output {value:1,Done:fasle}
+console.log(iterator.next()); //output {value:2,Done:fasle}
+console.log(iterator.next()); //output {value:3,Done:fasle}
+console.log(iterator.next()); //output {value:undefined,Done:true}
+
+
+
+
+
