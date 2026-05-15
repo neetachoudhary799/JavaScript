1cb7d87ff574ac1da17b85c6b5bda28654dd8243
neeta <neeta.rani@mail.vinove.com>
2026-05-15 15:18:10 +0530

    iterators...

--- Medium/7_generator_iterators.js
+++ Medium/7_generator_iterators.js
@@ -12,6 +12,30 @@ console.log(iterator.next()); //output {value:3,Done:fasle}
 console.log(iterator.next()); //output {value:undefined,Done:true}
 
 
-//Generator : 
+//Generator : A Generator is a special function that can:
+//pause execution
+//resume later
+//use function *
+//A generator is a special function that can pause and resume execution using the yield keyword.
+//function*	generator function
+//yield	pause execution
+//next()	resume execution
+//done: true
+//means Generator finished ✔️
+//output shows {value:"Hello",Done:true/fasle}
 
+
+function* greet() {
+    console.log("Start");
+    yield "Hello";
+    console.log("Middle");
+    yield "Neeta";
+    console.log("End");
+}
+
+let gen = greet();
+
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
 
