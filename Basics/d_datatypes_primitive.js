bbfb948048b0665f974b7bae02c4aff976c4b898
neeta <neeta.rani@mail.vinove.com>
2026-06-16 11:20:09 +0530

    Data types...

--- Basics/d_datatypes_primitive.js
+++ Basics/d_datatypes_primitive.js
@@ -1,3 +1,5 @@
+//datatypes
+// 1. Primitive Data Types
 //primitive data types: string, number, boolean, null, undefined, symbol,bigint
 
 //string
@@ -29,3 +31,23 @@ console.log(typeof sym); //output: symbol
 //bigint
 let bigIntValue = 1234567890123456789012345678901234567890n;
 console.log(typeof bigIntValue); //output: bigint
+
+
+// 2. Non-Primitive Data Types
+// 2.1 Object
+let obj = {
+    name: "Neeta Rani",
+    age: 25
+};
+console.log(typeof obj); // Output: "object"
+
+// 2.2 Array
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof arr); // Output: "object" (arrays are a type of object in JavaScript)
+
+// 2.3 Function
+function greet() {  
+    console.log("Hello!");
+}
+greet(); // Output: "Hello!"
+console.log(typeof greet); // Output: "function"
